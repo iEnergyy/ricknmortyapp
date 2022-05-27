ad39a8d842dfc511247d0b7a005b08c463ed2ae1
DESKTOP-97JL9E4\mhenriquez <miguel.henriquez323@gmail.com>
2022-05-26 23:46:10 -0400

    modified the getCharacters method so it returns all 800+ characters

--- lib/services/characterService.dart
+++ lib/services/characterService.dart
@@ -4,12 +4,24 @@ import '../models/character.dart';
 import 'package:http/http.dart' as http;
 
 Future<List<Character>> getCharacters() async {
-  final response =
+  List<Character> AllCharacters = [];
+  final getPages =
       await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
-
-  if (response.statusCode == 200) {
-    List jsonResponse = json.decode(response.body)['results'];
-    return jsonResponse.map((data) => Character.fromJson(data)).toList();
+  if (getPages.statusCode == 200) {
+    int charactersPages = json.decode(getPages.body)['info']['pages'];
+    for (int i = 1; i <= charactersPages; i++) {
+      final response = await http.get(Uri.parse(
+          'https://rickandmortyapi.com/api/character?page=${i.toString()}'));
+      if (response.statusCode == 200) {
+        List jsonResponse = json.decode(response.body)['results'];
+        List<Character> charactersFromPage =
+            jsonResponse.map((data) => Character.fromJson(data)).toList();
+        AllCharacters.addAll(charactersFromPage);
+      } else {
+        throw Exception('Failed to load characters');
+      }
+    }
+    return AllCharacters;
   } else {
     throw Exception('Failed to load characters');
   }
