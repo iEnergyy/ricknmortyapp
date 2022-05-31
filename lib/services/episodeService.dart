4f2bcd90b1f2c25835253463e6405ef6f5d39dae
DESKTOP-97JL9E4\mhenriquez <miguel.henriquez323@gmail.com>
2022-05-31 00:39:34 -0400

    episodes working no comments

--- lib/services/episodeService.dart
+++ lib/services/episodeService.dart
@@ -16,27 +16,14 @@ Future<List<Episode>> getEpisodesWithURL(List<String> episodeUrls) async {
   if (episodesResponse.statusCode == 200) {
     late List<Episode> episodes = [];
     if (episodeUrls.length == 1) {
-      // List<Episode> episode = jsonResponse.cast<Episode>().toList();
-      Map<String, dynamic> jsonResponse =
-          jsonDecode(episodesResponse.body); //good
-      // episodes.add(episode[0]);
-      // final episode = jsonResponse as Map<String, dynamic>;
+      Map<String, dynamic> jsonResponse = jsonDecode(episodesResponse.body);
       final episode = jsonResponse;
-      // episodes.add(Episode.fromJson(episode));
       episodes.add(Episode.fromJson(episode));
     } else {
-      List jsonResponse = jsonDecode(episodesResponse.body); //good
+      List jsonResponse = jsonDecode(episodesResponse.body);
       episodes = jsonResponse.map((x) => Episode.fromJson(x)).toList();
     }
-
-    // List<Episode> episodes2 = episodeUrls.length > 1
-    //     ? jsonResponse.cast<Episode>().toList()
-    //     : jsonResponse.map((x) => Episode.fromJson(x)).toList();
-
-    // List<Episode> episodes =
-    //     jsonResponse.map((x) => Episode.fromJson(x)).toList();
     return episodes;
-    // return episodes2;
   } else {
     throw Exception('Failed to load episodes');
   }
