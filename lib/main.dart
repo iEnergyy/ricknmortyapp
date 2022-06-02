48e75d4dbfae4f0551273215e23113bfeac16f31
iEnergyy <44242768+iEnergyy@users.noreply.github.com>
2022-06-02 17:59:38 -0400

    Create detail page (#1)
    * Started creating the detail page for characters
    
    * modified the getCharacters method so it returns all 800+ characters
    
    * Added more fields, need to style them
    
    * deleted comments
    
    * finally getting episodes, need to fix it to work with 1 episode
    
    * episodes working, lots of comments
    
    * episodes working no comments
    
    * yoohooo
    
    * added more fields to the character details page
    
    * added type on detail screen
    
    * fixed a typo
    
    Co-authored-by: mhenriquez <miguel.henriquez323@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:rick_n_morty/models/episode.dart';
+import 'package:rick_n_morty/services/episodeService.dart';
 
 import 'models/character.dart';
 import 'services/characterService.dart';
@@ -93,26 +96,111 @@ class _HomePageState extends State<HomePage> {
       );
 }
 
-//TODO: Create a cool UI for the detail page.
-class DetailPage extends StatelessWidget {
-  const DetailPage({Key? key, required this.characterDetails})
-      : super(key: key);
+//TODO: Refactor.
+class DetailPage extends StatefulWidget {
+  DetailPage({Key? key, required this.characterDetails}) : super(key: key);
 
   final Character characterDetails;
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  Future<List<Episode>>? episodesFuture;
+
+  @override
+  void initState() {
+    episodesFuture = getEpisodesWithURL(widget.characterDetails.episode);
+    super.initState();
+  }
+
+  Color getStatusColor() {
+    switch (widget.characterDetails.status) {
+      case 'Alive':
+        return Colors.green;
+      case 'Dead':
+        return Colors.red;
+      default:
+        return Colors.yellow;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(characterDetails.name),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+        title: const Text(
+          'Character Details',
+          textAlign: TextAlign.center,
         ),
       ),
+      body: ListView(children: [
+        Image.network(widget.characterDetails.image,
+            width: double.infinity, height: 300, fit: BoxFit.cover),
+        ListTile(
+          title: Text(
+            widget.characterDetails.name,
+            textAlign: TextAlign.left,
+            style:GoogleFonts.roboto(fontSize: 30, fontWeight: FontWeight.w700))),
+        ListTile(
+          leading: Container(
+              height: 15,
+              width: 15,
+              decoration: BoxDecoration(
+                  color: getStatusColor(),
+                  borderRadius: BorderRadius.circular(100)),
+            ),
+          title: Text(
+                '${widget.characterDetails.status} - ${widget.characterDetails.species}'),
+          horizontalTitleGap: 0,
+        ),
+        ListTile(
+          title: widget.characterDetails.type == ''
+                  ? Text(widget.characterDetails.gender)
+                  : Text('${widget.characterDetails.gender} - ${widget.characterDetails.type}')
+        ),
+        ListTile(
+          title: Text(widget.characterDetails.origin.name),
+        ),
+        ExpansionTile(
+          title: const Text('Episodes'),
+          children: [
+            FutureBuilder<List<Episode>>(
+                future: episodesFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text(':( ${snapshot.error}');
+                  } else if (snapshot.hasData) {
+                    final episodes = snapshot.data!;
+
+                    return buildEpisodes(episodes);
+                  } else {
+                    return const Text('No characters :(');
+                  }
+                })
+          ],
+          ),
+        
+      ]),
     );
   }
+
+  Widget buildEpisodes(List<Episode> episodes) => ListView.builder(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemCount: episodes.length,
+      itemBuilder: (context, index) {
+        final episode = episodes[index];
+
+        return Card(
+          child: ListTile(
+            title: Text(episode.name),
+            subtitle: Text('${episode.episode} - ${episode.air_date}'),
+          ),
+        );
+      });
 }
+
---
+++ lib/services/episodeService.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:rick_n_morty/models/episode.dart';
+
+Future<List<Episode>> getEpisodesWithURL(List<String> episodeUrls) async {
+  List<String> episodeIds = [];
+  for (var url in episodeUrls) {
+    episodeIds.add(url.substring(url.lastIndexOf('/')).split('/').last);
+  }
+
+  String ids = episodeIds.join(',');
+  final episodesResponse = await http
+      .get(Uri.parse('https://rickandmortyapi.com/api/episode/${ids}'));
+
+  if (episodesResponse.statusCode == 200) {
+    late List<Episode> episodes = [];
+    if (episodeUrls.length == 1) {
+      Map<String, dynamic> jsonResponse = jsonDecode(episodesResponse.body);
+      final episode = jsonResponse;
+      episodes.add(Episode.fromJson(episode));
+    } else {
+      List jsonResponse = jsonDecode(episodesResponse.body);
+      episodes = jsonResponse.map((x) => Episode.fromJson(x)).toList();
+    }
+    return episodes;
+  } else {
+    throw Exception('Failed to load episodes');
+  }
+}
