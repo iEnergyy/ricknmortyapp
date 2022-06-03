44117e9f337cf9c030be447062fb4fc0b9c9fb93
mhenriquez <miguel.henriquez323@gmail.com>
2022-06-03 01:08:09 -0400

    created drawer

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,25 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: (){},
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: (){},
+            )
+            ],
+          ),
+        ),
         body: Center(
             child: FutureBuilder<List<Character>>(
           future: charactersFuture,
