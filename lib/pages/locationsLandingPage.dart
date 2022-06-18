33012df49a33578043c1a133de6d2cff3cbb7e23
MHenriquez <miguel.henriquez323@gmail.com>
2022-06-18 11:39:07 -0400

    added locations landing page and started detail page

--- lib/pages/locationsLandingPage.dart
+++ lib/pages/locationsLandingPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rick_n_morty/models/location.dart';
+import 'package:rick_n_morty/pages/locationDetailPage.dart';
+import 'package:rick_n_morty/services/locationService.dart';
 
 import '../widgets/drawerNav.dart';
 
@@ -10,6 +13,14 @@ class LocationsLandingPage extends StatefulWidget {
 }
 
 class _LocationsLandingPageState extends State<LocationsLandingPage> {
+  Future<List<Location>>? locationsFuture;
+  
+  @override
+  void initState(){
+    locationsFuture = getAllLocations();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +28,44 @@ class _LocationsLandingPageState extends State<LocationsLandingPage> {
           title: const Text('Locations'),
         ),
       drawer: const DrawerNav(),
+      body: Center(
+        child: FutureBuilder<List<Location>>(
+          future: locationsFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text(':( ${snapshot.error}');
+            } else if (snapshot.hasData) {
+              final locations = snapshot.data!;
+
+              return buildLocations(locations);
+            } else {
+              return const Text('No characters :(');
+            }
+          },
+        )
+      ),
     );
   }
 }
+
+  Widget buildLocations(List<Location> locations) => ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          final location = locations[index];
+
+          return Card(
+            child: ListTile(
+              title: Text(location.name),
+              subtitle: Text(location.type),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: ((context) => LocationDetailPage())));
+              },
+            ),
+          );
+        },
+      );
